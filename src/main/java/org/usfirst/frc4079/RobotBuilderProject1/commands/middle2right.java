32fae5910f3a500523c5ab869d248aab0a160a7d
JonathanTran4079 <46210704+JonathanTran4079@users.noreply.github.com>
2018-12-29 15:07:19 -0800

    Update middle2right.java

--- src/main/java/org/usfirst/frc4079/RobotBuilderProject1/commands/middle2right.java
+++ src/main/java/org/usfirst/frc4079/RobotBuilderProject1/commands/middle2right.java
@@ -7,6 +7,7 @@ public class middle2right extends CommandGroup {
        //robot length is approx 33 inches, distance from switch to wall is around 140 inches so travel distance is arouund 110 inches
        
        public middle2right(){
+            /*
             //robot will travel 100 inches (5 sec) forward while simultaneously lowering intake wrist (0.3 sec)
             addParallel(new RaiseWrist(-0.3, .3));
             addSequential(new DriveDistance(100, 2.5));
@@ -20,6 +21,23 @@ public class middle2right extends CommandGroup {
             //drive forward and drop cube
             addParallel(new EjectCube(.5, 3));
             addSequential(new DriveDistance(10, 2));
+            */
+
+            //alternative method that may be quicker
+            //robot distance to switch is approx 110 inches and middle to center of swtich is around 54 inches
+            //robot lowers wrist while simultaneously slightly moving off of wall
+            addParallel(new RaiseWrist(-0.3, .3));
+            addSequential(new DriveDistance(5, 1));
+            //robot rotates to face the right switch
+            addSequential(new TurnByAngle(26, 2));
+            //robot drives distance towards switch
+            addSequential(new DriveDistance(120, 5));
+            //robot turns back to face switch while lifting elevator
+            addParallel(new RaiseElevator(.5, 2));
+            addSequential(new TurnByAngle(-26,2));
+            //robot drives forward and drops cube
+            addParallel(new EjectCube(.5, 3));
+            addSequential(new DriveDistance(5, 2));
 
        }
 
