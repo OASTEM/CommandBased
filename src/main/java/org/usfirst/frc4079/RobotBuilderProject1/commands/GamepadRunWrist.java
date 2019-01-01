ec89bf235568c5315fcaaad3e78c4027cc246377
Justin Yue <flyingjay16@gmail.com>
2018-12-31 22:53:03 -0800

    GamepadRunWrist
    Created command to run the wrist
    
    YButton moves wrist up at half speed
    AButton moves wrist down at half speed

---
+++ src/main/java/org/usfirst/frc4079/RobotBuilderProject1/commands/GamepadRunWrist.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc4079.RobotBuilderProject1.commands;
+
+import org.usfirst.frc4079.RobotBuilderProject1.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class GamepadRunWrist extends Command {
+  public GamepadRunWrist() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.wrist);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if(Robot.oi.gamePad.getYButton()){
+      Robot.wrist.run(0.5);
+    }
+    else if(Robot.oi.gamePad.getAButton()){
+      Robot.wrist.run(-0.5);
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.wrist.stop();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
